bf72dbe27ea72c890e307fedbab3161a13eb7da8
김재원 <kw2282222@gmail.com>
2022-09-01 23:44:53 +0900

    push

---
+++ algorithm/hash/3. 베스트앨범.js
@@ -0,0 +1,9 @@
+const genres = ['classic', 'pop', 'classic', 'classic', 'pop'];
+const plays = [500, 600, 150, 800, 2500];
+
+function solution(genres, plays) {
+    const arr = [];
+    genres.forEach((genre, index) => {});
+}
+
+console.log(solution(genres, plays));
