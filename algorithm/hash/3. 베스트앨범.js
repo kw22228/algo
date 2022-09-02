2819533ba32f3ee3860fcbf2ddbd1c93a77100a6
kw22228 <hot5521@naver.com>
2022-09-02 18:10:08 +0900

    hash algorithm

--- algorithm/hash/3. 베스트앨범.js
+++ algorithm/hash/3. 베스트앨범.js
@@ -2,8 +2,27 @@ const genres = ['classic', 'pop', 'classic', 'classic', 'pop'];
 const plays = [500, 600, 150, 800, 2500];
 
 function solution(genres, plays) {
-    const arr = [];
-    genres.forEach((genre, index) => {});
+    const answer = [];
+    const musicMap = new Map();
+
+    genres.forEach((genre, index) => {
+        if (!musicMap.has(genre)) musicMap.set(genre, 0);
+
+        musicMap.set(genre, musicMap.get(genre) + plays[index]);
+    });
+
+    const sortArr = [...musicMap].sort(([, a], [, b]) => b - a).map(item => item[0]);
+
+    sortArr.forEach(item => {
+        const genreIndexArr = [];
+        genres.forEach((genre, index) => {
+            if (genre === item) genreIndexArr.push([index, plays[index]]);
+        });
+        const sortGenreIndexarr = genreIndexArr.sort(([, a], [, b]) => b - a).slice(0, 2);
+        sortGenreIndexarr.forEach(([index]) => answer.push(index));
+    });
+
+    return answer;
 }
 
 console.log(solution(genres, plays));
