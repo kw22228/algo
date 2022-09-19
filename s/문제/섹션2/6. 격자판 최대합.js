78741062bd3ceda6d3247e6f5dd524944b1b7744
김재원 <kw2282222@gmail.com>
2022-09-19 13:05:55 +0900

    push

--- s/문제/섹션2/6. 격자판 최대합.js
+++ s/문제/섹션2/6. 격자판 최대합.js
@@ -5,3 +5,31 @@ let arr = [
     [19, 27, 29, 37, 27],
     [19, 13, 30, 13, 19],
 ];
+
+function solution(arr) {
+    let sum = 0;
+
+    //1. 가로 합
+    const answer1 = [];
+    arr.forEach((a, index) => {
+        sum = a.reduce((pre, cur) => pre + cur, 0);
+        answer1.push(sum);
+    });
+
+    //2. 세로 합
+    const answer2 = Array.from({ length: arr[0].length }, () => Number(0));
+    arr.forEach((a, index) => {
+        a.forEach((n, i) => {
+            answer2[i] += n;
+        });
+    });
+
+    //3, 대각선
+    const answer3 = [];
+
+    const answer = [...answer1, ...answer2];
+
+    console.log(answer);
+}
+
+console.log(solution(arr));
