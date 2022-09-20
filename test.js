b7efefee20383317f143e5076a91bef950b0d5aa
kw22228 <hot5521@naver.com>
2022-09-20 13:35:07 +0900

    t

--- test.js
+++ test.js
@@ -1,18 +1,30 @@
-import InsertNode from './algorithm/tree/binarySearchTree/insertNode.js';
+// import InsertNode from './algorithm/tree/binarySearchTree/insertNode.js';
 
-const binarySearchTree = new InsertNode();
+// const binarySearchTree = new InsertNode();
 
-binarySearchTree.insert('10');
-binarySearchTree.insert('5');
-binarySearchTree.insert('4');
-binarySearchTree.insert('6');
-binarySearchTree.insert('11');
-binarySearchTree.insert('20');
-binarySearchTree.insert('8');
-binarySearchTree.insert('7');
-binarySearchTree.insert('5');
-binarySearchTree.insert('5');
-binarySearchTree.insert('5');
-binarySearchTree.insert('5');
+// binarySearchTree.insert('10');
+// binarySearchTree.insert('5');
+// binarySearchTree.insert('4');
+// binarySearchTree.insert('6');
+// binarySearchTree.insert('11');
+// binarySearchTree.insert('20');
+// binarySearchTree.insert('8');
+// binarySearchTree.insert('7');
+// binarySearchTree.insert('5');
+// binarySearchTree.insert('5');
+// binarySearchTree.insert('5');
+// binarySearchTree.insert('5');
 
-console.log(binarySearchTree);
+// console.log(binarySearchTree);
+
+setTimeout(() => {
+    console.log(1);
+}, 1000);
+
+setTimeout(() => {
+    console.log(2);
+}, 1000);
+
+setTimeout(() => {
+    console.log(3);
+}, 1000);
