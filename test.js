ce32e9fbd04dc99f7e85616fa3fc42d0cb2c5108
kw22228 <hot5521@naver.com>
2022-09-21 18:00:08 +0900

    tt

--- test.js
+++ test.js
@@ -17,14 +17,4 @@
 
 // console.log(binarySearchTree);
 
-setTimeout(() => {
-    console.log(1);
-}, 1000);
-
-setTimeout(() => {
-    console.log(2);
-}, 1000);
-
-setTimeout(() => {
-    console.log(3);
-}, 1000);
+const a = 'a';
